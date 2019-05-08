de75dbcc882bcc900eed53070427b95f6b167f59
ZhangJiaQiao <1254860908@qq.com>
2019-05-08 11:42:55 +0800

    修正ftree_test的UpdateTest的错误

--- Programming-FPTree/test/fptree_test.cpp
+++ Programming-FPTree/test/fptree_test.cpp
@@ -83,8 +83,11 @@ TEST(FPTreeTest, UpdateTest) {
     delete tree;
     tree = NULL;
     tree = new FPTree(2);
-    for (int i = 1; i <= LEAF_DEGREE; i++) {
+    for (int i = 1; i <= LEAF_DEGREE; i+=2) {
         EXPECT_EQ(tree->find(i), i * 200);
+    }
+    for (int i = 2; i <= LEAF_DEGREE; i+=2) {
+        EXPECT_EQ(tree->find(i), i * 100);
     }
     delete tree;
     tree = NULL;
